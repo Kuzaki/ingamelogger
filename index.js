85e09ba082be5492630ec82b5fb4c54818f855c1
SerenTera <SerenTera@users.noreply.github.com>
2017-06-05 02:50:09 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+module.exports = function iglogger(dispatch) {
+	let msgstr,
+		txt;
+	let	version=1,
+		count=1,
+		textstring='';
+		
+
+	dispatch.hook('C_CHAT', 1, event => {
+		if(event.message.includes('!logger')) {
+			if((/\d/.test(event.message))) {
+				version= parseInt(event.message.replace(/[^0-9\.]/g, '')),
+				message('Logging version '+(version));
+			}
+			else if(event.message.includes('!logger start')) {
+				if((/\d/.test(event.message))) {count= parseInt(event.message.replace(/[^0-9\.]/g, ''));},
+				hooker();
+			}
+			else
+				txt = event.message,
+				msgstr=txt.replace('</FONT>','').replace('<FONT>','').replace('!logger','').replace(' ',''),
+				message('Logging '+(msgstr)+' '+(version));	
+			return false;		
+		};
+	});
+			
+	function message(msg) {
+		dispatch.toClient('S_CHAT', 1, {
+			channel: 24,
+			authorID: 0,
+			unk1: 0,
+			gm: 0,
+			unk2: 0,
+			authorName: '',
+			message: msg
+		})
+	};
+	
+	function hooker() {
+		for(i = 0; i < count; i++) {
+			dispatch.hookOnce(msgstr, version, event => {
+				textstring= JSON.stringify(event),
+				message(msgstr+version+': '+textstring);
+			});
+		};
+    };
+};
