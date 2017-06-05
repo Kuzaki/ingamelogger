4e486ee8d8034409ca890a55b2c15bc7029b53cd
Seren <SerenTera@users.noreply.github.com>
2017-06-06 02:02:09 +0800

    New logger

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
+const fs = require('fs');
 module.exports = function iglogger(dispatch) {
 	let msgstr,
 		txt;
-	let	version=1,
+	let	version=1, //default packet version
+		i=1,
 		count=1,
+		writeto=true,
+		shush=false,
+		orderno= -999,
 		textstring='';
 		
 
 	dispatch.hook('C_CHAT', 1, event => {
 		if(event.message.includes('!logger')) {
-			if((/\d/.test(event.message))) {
-				version= parseInt(event.message.replace(/[^0-9\.]/g, '')),
+			if(event.message.includes('!logger start')) {
+				if((/\d/.test(event.message))) {
+					count= parseInt((event.message.replace(/[^0-9\.]/g, '')),10);
+				};
+				i=1;
+				message('Start log '+(msgstr)+(version)+' count: '+(count)),
+				hooker(count);
+			}
+			else if(event.message.includes('!logger order')) {
+				orderno= parseInt((event.message.replace(/[^0-9\.]/g, '')),10),
+				message('Logging order '+orderno)
+			}			
+			
+			else if(event.message.includes('shush')) {
+				if(!shush) {
+					shush=true, 
+					message('Logger system message disabled');
+				}
+				else 
+					shush=false, 
+					message('Logger system message enabled');
+			}
+			
+			else if((/\d/.test(event.message)) ||(event.message.includes('*'))) {
+				if (event.message.includes('*')) {version='*';}
+				else
+					version= parseInt(event.message.replace(/[^0-9\.]/g, ''));
 				message('Logging version '+(version));
 			}
-			else if(event.message.includes('!logger start')) {
-				if((/\d/.test(event.message))) {count= parseInt((event.message.replace(/[^0-9\.]/g, '')),10);};
-				message('Start log '+(msgstr)+(version)+' count: '+(count)),
-				hooker();
+			else if(event.message.includes('!logger save')) {
+				if(!writeto) {
+					writeto=true, 
+					message('Saving Logs enabled');
+				}
+				else 
+					writeto=false, 
+					message('Saving Logs disabled');
+			}
+			else if(event.message.includes('!logger raw')) 	{
+				hookall(),
+				message('Logging raw packets, Order:'+orderno);		
 			}
 			else
 				txt = event.message,
 				msgstr=txt.replace('</FONT>','').replace('<FONT>','').replace('!logger','').replace(' ',''),
-				message('Logging '+(msgstr)+' '+(version));	
+				message('Logging '+(msgstr)+' '+(version)+' Order:'+orderno);	
 			return false;		
 		};
 	});
@@ -34,15 +72,33 @@ module.exports = function iglogger(dispatch) {
 			unk2: 0,
 			authorName: '',
 			message: msg
-		})
+		});
 	};
 	
-	function hooker() {
-		for(i = 0; i < count; i++) {
-			dispatch.hookOnce(msgstr, version, event => {
-				textstring= JSON.stringify(event),
-				message('['+msgstr+version+'] '+(i)+' : '+textstring);
-			});
-		};
-    };
+	function hooker(counter) {
+		if(i<=counter) {
+			dispatch.hookOnce(msgstr, version, {order: (orderno)}, event => {
+				textstring= JSON.stringify(event);
+				if(!shush) {message('['+msgstr+version+'] '+(i)+' : '+textstring);};
+				if(writeto) {
+					let datehour=JSON.stringify(Date()).slice(5,20).replace(':','h'),
+					minutesec=JSON.stringify(Date()).slice(18,25);
+					fs.appendFileSync(((__filename).replace('index.js',('log '+datehour+'.json'))),('['+minutesec+'] ['+msgstr+version+'] '+(i)+' : '+textstring+'\r\n'))
+				};
+				i++,
+				hooker(count);
+			});			
+		}
+		else
+			message('Finished logging');
+	};		
+	
+	function hookall() {
+		dispatch.hook('*', 'raw', {order: (orderno)}, (code, data, fromServer) => {
+			let rawtext=(fromServer ? 'Yes' : 'No', dispatch.base.protocolMap.code.get(code), data.toString('hex'));
+			let datehour=JSON.stringify(Date()).slice(5,20).replace(':','h'),
+			minutesec=JSON.stringify(Date()).slice(18,25);
+			fs.appendFileSync(((__filename).replace('index.js',('log_raw '+datehour+'.json'))),('['+minutesec+']'+': '+'From server? '+rawtext[0]+' ('+rawtext[1]+') '+rawtext[2]+'\r\n'));
+		});
+	};
 };
