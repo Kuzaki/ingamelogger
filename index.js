ed5dab84e8dce1d7afad0409462e96b9dce9dd4a
Seren <SerenTera@users.noreply.github.com>
2017-07-14 23:38:18 +0800

    Add console logging

--- index.js
+++ index.js
@@ -1,104 +1,115 @@
-const fs = require('fs');
-module.exports = function iglogger(dispatch) {
-	let msgstr,
-		txt;
-	let	version='*',	//default packet version
-		writeto=true, //default write to log file (true to save logs)
-		shush=false,  //default silence system messages (true to silence sys msg)
-		orderno= -999, //default order of packet (refer to tera-proxy-game readme)
-		count=1,
-		textstring='';
-		
+const fs = require('fs'),
+	path= require('path'),
+	Command = require('command')
 
-	dispatch.hook('C_CHAT', 1, event => {
-		if(event.message.includes('!logger')) {
-			if(event.message.includes('!logger start')) {
-				if((/\d/.test(event.message))) {
-					count= parseInt((event.message.replace(/[^0-9\.]/g, '')),10);
-				};
-				message('Start log '+(msgstr)+(version)+' count: '+(count)),
-				hooker(count,msgstr,version,orderno,1);
-			}
-			else if(event.message.includes('!logger order')) {
-				orderno= parseInt((event.message.replace(/[^0-9\.\-]/g, '')),10), //update negative value
-				message('Logging order '+orderno)
-			}			
-			
-			else if(event.message.includes('shush')) {
-				if(!shush) {
-					shush=true, 
-					message('Logger system message disabled');
-				}
-				else 
-					shush=false, 
-					message('Logger system message enabled');
-			}
-			
-			else if(event.message.includes('!logger version')) {
-				if (event.message.includes('*')) {version='*';}
-				else
-					version= parseInt(event.message.replace(/[^0-9\.]/g, ''));
-				message('Logging version '+(version));
-			}
-			else if(event.message.includes('!logger save')) {
-				if(!writeto) {
-					writeto=true, 
-					message('Saving Logs enabled');	
-				}
-				else 
-					writeto=false, 
-					message('Saving Logs disabled');
-			}
-			else if(event.message.includes('!logger raw')) 	{
-				let pkt=(event.message.replace('!logger raw', '').replace(' ','')).replace('</FONT>','').replace('<FONT>','');
-				if(pkt==='') {pkt='*';};
-				hookall(pkt),
-				message('Logging raw '+pkt+' packets. Order:'+orderno);		
-			}
-			else
-				txt = event.message,
-				msgstr=txt.replace('</FONT>','').replace('<FONT>','').replace('!logger','').replace(' ',''),
-				message('Logging '+(msgstr)+' '+(version)+' Order:'+orderno);	
-			return false;		
-		};
-	});
-			
-	function message(msg) {
-		dispatch.toClient('S_CHAT', 1, {
-			channel: 24,
-			authorID: 0,
-			unk1: 0,
-			gm: 0,
-			unk2: 0,
-			authorName: '',
-			message: msg
-		});
-	};
+//Defaults
+let version = '*',	//default packet version
+	writeto = true, //default write to log file (true to save logs)
+	sysmsg = true, //default silence system messages (false to silence sys msg)
+	consolelog = false,//default console logging (false to silence console logging)
+	orderno = -999 //default order of packet (refer to tera-proxy-game readme)
 	
+module.exports = function iglogger(dispatch) {
+	const command = Command(dispatch)
+	
+	let msgstr,
+		txt,
+		count = 1,
+		textstring = ''
+		
+	command.add('loggerstart',count => {
+		count=parseInt(count),
+		command.message('Start log '+(msgstr)+(version)+' count: '+(count)),
+		hooker(count,msgstr,version,orderno,1)
+	})
+	
+	command.add('loggerconsole',() => {
+		if(!consolelog) {
+			consolelog=true,
+			command.message('Logger console logs enabled')
+		}
+		else
+			consolelog=false, 
+			message('Logger console logs disabled')
+	})
+
+	
+	command.add('loggerorder',order => {
+		orderno= parseInt(orderno), //update negative value
+		command.message('Logging order '+orderno)
+	})
+	
+	command.add('loggersysmsg', () => {
+		if(!sysmsg) {
+			sysmsg=true,
+			command.message('Logger system message enabled')
+		}
+		else
+			sysmsg=false, 
+			message('Logger system message disabled')
+	})
+
+	command.add('loggerversion', versionno => {
+		version= parseInt(versionno)
+		if(isNaN(version)) {
+			version='*',
+			command.message('Logging latest version *')
+		}
+		else
+			version= parseInt(versionno),
+			command.message('Logging version '+version)
+	})
+	
+	command.add('loggersave',() => {
+		if(!writeto) {
+			writeto=true, 
+			command.message('Saving Logs enabled')
+		}
+		else 
+			writeto=false, 
+			command.message('Saving Logs disabled')
+	})
+	
+	command.add('loggerraw', pkt => {
+		if(pkt===undefined) {
+			pkt='*'
+		}
+		hookall(pkt),
+		command.message('Logging raw '+pkt+' packets. Order:'+orderno)
+	})
+
+	command.add('logger', packet => {
+		command.message('Logging '+(msgstr)+' '+(version)+' Order:'+orderno)
+	})
+
 	function hooker(counter,msgstring,ver,ordern,id) {
 		if(id<=counter) {
 			dispatch.hookOnce(msgstring, ver, {order: (ordern)}, event => {
-				textstring= JSON.stringify(event);
-				if(!shush && msgstring!=='S_CHAT') {message('['+msgstring+ver+'] '+(id)+' : '+textstring);};
+				textstring= JSON.stringify(event)
+				if(sysmsg) 
+					command.message('['+msgstring+ver+'] '+(id)+' : '+textstring);
+				if(consolelog)
+					console.log('['+msgstring+ver+'] '+(id)+' : '+textstring);
 				if(writeto) {
-					let datehour=JSON.stringify(Date()).slice(5,20).replace(':','h'),
-					minutesec=JSON.stringify(Date()).slice(18,25);
-					fs.appendFileSync(((__filename).replace('index.js',('log '+datehour+'.json'))),('['+minutesec+'] ['+msgstring+' '+ver+']('+ordern+') '+(id)+' : '+textstring+'\r\n'))
-				};
+					let datehour=Date().slice(5,20).replace(':','h'),
+					minutesec=Date().slice(18,25);
+					fs.appendFileSync(path.join(__dirname,('log '+datehour+'.json')),('['+minutesec+'] ['+msgstring+' '+ver+']('+ordern+') '+(id)+' : '+textstring+'\r\n'))
+				}
 				id++,
-				hooker(count,msgstring,ver,ordern,id);
-			});			
+				hooker(count,msgstring,ver,ordern,id)
+			})
 		}
 		else
-			message('Finished logging');
-	};		
+			command.message('Finished logging')
+	}	
 	
 	function hookall(pktname) {
 		dispatch.hook(pktname, 'raw', {order: (orderno)}, (code, data, fromServer) => { 
-			let rawtext={x:(fromServer ? 'Yes' : 'No'), y: (dispatch.base.protocolMap.code.get(code)), z: (data.toString('hex'))};
-			let datehour=JSON.stringify(Date()).slice(5,20).replace(':','h'),
-			minutesec=JSON.stringify(Date()).slice(18,25);
-			fs.appendFileSync(((__filename).replace('index.js',('log_raw '+pktname.replace('*','All')+' '+datehour+'.json'))),('['+minutesec+']'+': '+'From server? '+(rawtext.x)+' ('+(rawtext.y)+') '+(rawtext.z)+'\r\n'));
-		});
-	};
-};
+			let rawtext={x:(fromServer ? 'Yes' : 'No'), y: (dispatch.base.protocolMap.code.get(code)), z: (data.toString('hex'))},
+				datehour=Date().slice(5,20).replace(':','h'),
+				minutesec=Date().slice(18,25);
+			console.log('['+minutesec+']'+': '+'From server? '+rawtext.x+' ('+rawtext.y+') '+rawtext.z),
+			fs.appendFileSync(path.join(__dirname,('log_raw '+pktname.replace('*','All')+' '+datehour+'.json')),('['+minutesec+']'+': '+'From server? '+(rawtext.x)+' ('+(rawtext.y)+') '+(rawtext.z)+'\r\n'))
+		})
+	}
+}
