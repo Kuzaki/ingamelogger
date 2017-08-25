b90006a959252405fd44e42990a21c4a745db8b8
Seren <SerenTera@users.noreply.github.com>
2017-08-26 01:16:26 +0800

    Opcode in place of unmatched opcodes for raw hooks

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const fs = require('fs'),
 	Command = require('command')
 
 //Defaults
-let version = '*',	//default packet version
-	writeto = true, //default write to log file (true to save logs)
-	sysmsg = true, //default silence system messages (false to silence sys msg)
-	consolelog = false,//default console logging (false to silence console logging)
-	orderno = -999 //default order of packet (refer to tera-proxy-game readme)
+let version = '*',		//default packet version
+	writeto = true, 	//default write to log file (true to save logs)
+	sysmsg = true, 		//default silence system messages (false to silence sys msg)
+	consolelog = false,	//default console logging (false to silence console logging)
+	orderno = -999 		//default order of packet (refer to tera-proxy-game readme)
 	
 module.exports = function iglogger(dispatch) {
 	const command = Command(dispatch)
@@ -16,10 +16,12 @@ module.exports = function iglogger(dispatch) {
 		txt,
 		count = 1,
 		textstring = ''
-		
+	
+	
+	//////Commands
 	command.add('loggerstart',counts => {
-		if(counts!==undefined) count=parseInt(counts);
-		command.message('Start log '+(msgstr)+(version)+' count: '+(count)),
+		if(counts!==undefined) count=parseInt(counts)
+		command.message('Start log '+(msgstr)+(version)+' count: '+(count))
 		hooker(count,msgstr,version,orderno,1)
 	})
 	
@@ -29,13 +31,8 @@ module.exports = function iglogger(dispatch) {
 	})
 	
 	command.add('loggersysmsg', () => {
-		if(!sysmsg) {
-			sysmsg=true,
-			command.message('Logger system message enabled')
-		}
-		else
-			sysmsg=false, 
-			command.message('Logger system message disabled')
+		sysmsg=!sysmsg
+		command.message(sysmsg ? 'Logger system message enabled' : 'Logger system message disabled')
 	})
 
 	command.add('loggerversion', versionno => {
@@ -50,53 +47,45 @@ module.exports = function iglogger(dispatch) {
 	})
 	
 	command.add('loggersave',() => {
-		if(!writeto) {
-			writeto=true, 
-			command.message('Saving Logs enabled')
-		}
-		else 
-			writeto=false, 
-			command.message('Saving Logs disabled')
+		writeto=!writeto
+		command.message(writeto ? 'Saving logs enabled' : 'Saving logs disabled')
 	})
 	
 	command.add('loggerconsole',() => {
-		if(!consolelog) {
-			consolelog=true,
-			command.message('Logger console logs enabled')
-		}
-		else
-			consolelog=false, 
-			command.message('Logger console logs disabled')
+		consolelog=!consolelog
+		command.message(consolelog ? 'Logger console logs enabled' : 'Logger console logs disabled')
 	})
 
 	
 	command.add('loggerraw', pkt => {
-		if(pkt===undefined) {
-			pkt='*'
-		}
-		hookall(pkt),
+		if(pkt===undefined) pkt='*'
+		hookall(pkt)
 		command.message('Logging raw '+pkt+' packets. Order:'+orderno)
 	})
 
 	command.add('logger', packet => {
-		msgstr=packet,
+		msgstr=packet
 		command.message('Logging '+(msgstr)+' '+(version)+' Order:'+orderno)
 	})
-
+	
+	
+	
+	//////Functions
 	function hooker(counter,msgstring,ver,ordern,id) {
 		if(id<=counter) {
-			dispatch.hookOnce(msgstring, ver, {order: (ordern)}, event => {
+			dispatch.hookOnce(msgstring, ver, {order: (ordern)}, event => {  ///TODO: Use unhook to handle.
 				textstring= JSON.stringify(event)
-				if(sysmsg) 
-					command.message('['+msgstring+ver+'] '+(id)+' : '+textstring);
-				if(consolelog)
-					console.log('['+msgstring+ver+'] '+(id)+' : '+textstring);
+				if(sysmsg) command.message('['+msgstring+ver+'] '+(id)+' : '+textstring)
+					
+				if(consolelog) console.log('['+msgstring+ver+'] '+(id)+' : '+textstring)
+					
 				if(writeto) {
 					let datehour=Date().slice(4,19).replace(':','h'),
-					minutesec=Date().slice(16,24);
+					minutesec=Date().slice(16,24)
 					fs.appendFileSync(path.join(__dirname,('log '+datehour+'.json')),('['+minutesec+'] ['+msgstring+' '+ver+']('+ordern+') '+(id)+' : '+textstring+'\r\n'))
 				}
-				id++,
+				
+				id++
 				hooker(count,msgstring,ver,ordern,id)
 			})
 		}
@@ -108,9 +97,9 @@ module.exports = function iglogger(dispatch) {
 		dispatch.hook(pktname, 'raw', {order: (orderno)}, (code, data, fromServer) => { 
 			let rawtext={x:(fromServer ? 'Yes' : 'No'), y: (dispatch.base.protocolMap.code.get(code)), z: (data.toString('hex'))},
 				datehour=Date().slice(4,19).replace(':','h'),
-				minutesec=Date().slice(16,24);
-			if(consolelog) console.log('['+minutesec+']'+': '+'From server? '+rawtext.x+' ('+rawtext.y+') '+rawtext.z);
-			fs.appendFileSync(path.join(__dirname,('log_raw '+pktname.replace('*','All')+' '+datehour+'.json')),('['+minutesec+']'+': '+'From server? '+(rawtext.x)+' ('+(rawtext.y)+') '+(rawtext.z)+'\r\n'))
+				minutesec=Date().slice(16,24)
+			if(consolelog) console.log('['+minutesec+']'+': '+'From server? '+rawtext.x+' ('+rawtext.y===undefined ? code : rawtext.y+') '+rawtext.z)
+			fs.appendFileSync(path.join(__dirname,('log_raw '+pktname.replace('*','All')+' '+datehour+'.json')),('['+minutesec+']'+': '+'From server? '+(rawtext.x)+' ('+(rawtext.y===undefined ? code : rawtext.y)+') '+(rawtext.z)+'\r\n'))
 		})
 	}
 }
